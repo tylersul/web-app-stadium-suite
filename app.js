5f8f4659ede57d6d609b26aad3f2682be50c691e
Franchise <tylerreedsullivan@gmail.com>
2020-10-28 15:55:53 -0500

    added nodemon lib, edit stadium route

--- app.js
+++ app.js
@@ -20,11 +20,42 @@ db.once("open", () => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true}));
+
 // Routes
+// Root Routes
 app.get('/', (req, res) => {
     res.render('home');
 });
 
+// Stadium Routes
+app.get("/stadiums", async (req, res) => {
+    const stadiums = await Stadium.find({});
+    res.render('stadiums/index', { stadiums });
+});
+
+// Create New Stadium Routes
+app.get('/stadiums/new', (req, res) => {
+    res.render('stadiums/new');
+});
+
+app.post('/stadiums', async (req, res) => {
+    const stadium = new Stadium(req.body.stadium);
+    await stadium.save();
+    res.redirect(`/stadiums/${stadium._id}`);
+})
+
+app.get("/stadiums/:id", async (req, res) => {
+    const stadium = await Stadium.findById(req.params.id);
+    res.render("stadiums/show", { stadium });
+});
+
+app.get('/stadiums/:id/edit', async (req, res) => {
+    const stadium = await Stadium.findById(req.params.id);
+    res.render("stadiums/edit", { stadium });
+})
+
+// Listener
 app.listen(3000, () => {
     console.log('Serving app on port 3000');
 });
