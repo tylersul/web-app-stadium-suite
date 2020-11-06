a337d714f620b5fa45f40573aae54d06b81a8e73
Franchise <tylerreedsullivan@gmail.com>
2020-11-06 10:35:47 -0600

    Add stadium mw to also delete reviews when deleted

--- models/stadium.js
+++ models/stadium.js
@@ -16,4 +16,14 @@ const StadiumSchema = new Schema({
     ]
 });
 
+StadiumSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
+
 module.exports = mongoose.model('Stadium', StadiumSchema);
