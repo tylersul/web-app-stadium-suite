906633b4e2c13d63f08b48406e12b073a0c85019
Franchise <tylerreedsullivan@gmail.com>
2020-11-06 16:34:36 -0600

    Added route directory, refactored stadium routes.

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const Joi = require('joi');
 const Stadium  = require('./models/stadium'),
       Review   = require('./models/review');
 
+const stadiumRoutes = require('./routes/stadiumRoutes');
 
 mongoose.connect('mongodb://localhost:27017/stadium-suite', {
     useNewUrlParser: true,
@@ -32,18 +33,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true}));
 app.use(methodOverride('_method'));
 
-const validateStadium = (req, res, next) => {
-    
-    const { error } = stadiumJoiSchema.validate(req.body);
 
-    if(error) {
-        //details is array of objects
-        const message = error.details.map(el => el.message).join(',');
-        throw new ExpressError(message, 400);
-    } else {
-        next();
-    }
-}
 
 const validateReview = (req, res, next) => {
     
@@ -58,51 +48,13 @@ const validateReview = (req, res, next) => {
     }
 }
 
+app.use('/stadiums', stadiumRoutes);
+
 // Routes
 // Root Routes
 app.get('/', (req, res) => {
     res.render('home');
 });
-
-// Stadium Routes
-app.get("/stadiums", async (req, res) => {
-    const stadiums = await Stadium.find({});
-    res.render('stadiums/index', { stadiums });
-});
-
-// Create New Stadium Routes
-app.get('/stadiums/new', (req, res) => {
-    res.render('stadiums/new');
-});
-
-app.post('/stadiums', validateStadium, catchAsync(async (req, res) => {
-    // if(!req.body.stadium) throw new ExpressError('Invalid Stadium Data', 400);
-    const stadium = new Stadium(req.body.stadium);
-    await stadium.save();
-    res.redirect(`/stadiums/${stadium._id}`);
-}));
-
-app.get("/stadiums/:id", catchAsync(async (req, res) => {
-    const stadium = await Stadium.findById(req.params.id).populate('reviews');
-    res.render("stadiums/show", { stadium });
-}));
-
-app.get('/stadiums/:id/edit', catchAsync(async (req, res) => {
-    const stadium = await Stadium.findById(req.params.id);
-    res.render("stadiums/edit", { stadium });
-}));
-
-app.put('/stadiums/:id', validateStadium, catchAsync(async (req, res) => {
-    const { id } = req.params; //destructured
-    const stadium = await Stadium.findByIdAndUpdate(id, { ...req.body.stadium }) //spread
-    res.redirect(`/stadiums/${stadium._id}`);
-}));
-
-app.delete('/stadiums/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const stadium = await Stadium.findByIdAndDelete(id, { ...req.body.stadium});
-    res.redirect('/stadiums');
-}));
 
 app.post('/stadiums/:id/reviews', validateReview, catchAsync(async (req, res) => {
     const stadium = await Stadium.findById(req.params.id);
---
+++ routes/stadiumRoutes.js
@@ -0,0 +1,62 @@
+const express              = require('express'),
+      router               = express.Router(),
+      catchAsync           = require('../utils/catchAsync'),
+      ExpressError         = require('../utils/expressError'),
+      { stadiumJoiSchema } = require('../schemas.js')
+
+const Stadium  = require('../models/stadium');
+
+const validateStadium = (req, res, next) => {
+    
+    const { error } = stadiumJoiSchema.validate(req.body);
+
+    if(error) {
+        //details is array of objects
+        const message = error.details.map(el => el.message).join(',');
+        throw new ExpressError(message, 400);
+    } else {
+        next();
+    }
+}
+
+// Stadium Routes
+router.get('/', async (req, res) => {
+    const stadiums = await Stadium.find({});
+    res.render('stadiums/index', { stadiums });
+});
+
+// Create New Stadium Routes
+router.get('/new', (req, res) => {
+    res.render('stadiums/new');
+});
+
+router.post('/', validateStadium, catchAsync(async (req, res) => {
+    // if(!req.body.stadium) throw new ExpressError('Invalid Stadium Data', 400);
+    const stadium = new Stadium(req.body.stadium);
+    await stadium.save();
+    res.redirect(`/stadiums/${stadium._id}`);
+}));
+
+router.get("/:id", catchAsync(async (req, res) => {
+    const stadium = await Stadium.findById(req.params.id).populate('reviews');
+    res.render("stadiums/show", { stadium });
+}));
+
+router.get('/:id/edit', catchAsync(async (req, res) => {
+    const stadium = await Stadium.findById(req.params.id);
+    res.render("stadiums/edit", { stadium });
+}));
+
+router.put('/:id', validateStadium, catchAsync(async (req, res) => {
+    const { id } = req.params; //destructured
+    const stadium = await Stadium.findByIdAndUpdate(id, { ...req.body.stadium }) //spread
+    res.redirect(`/stadiums/${stadium._id}`);
+}));
+
+router.delete('/:id', catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const stadium = await Stadium.findByIdAndDelete(id, { ...req.body.stadium});
+    res.redirect('/stadiums');
+}));
+
+module.exports = router;
