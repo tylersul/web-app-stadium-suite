2ffe441d1f60ac9f22211d5a6a33a6b2cf8ef42a
Franchise <tylerreedsullivan@gmail.com>
2020-12-01 11:39:25 -0600

    Further explanatory comments

--- models/review.js
+++ models/review.js
@@ -1,9 +1,12 @@
+// Library Imports 
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
+// Schema definition
 const ReviewSchema = new Schema({
     body: String,
     rating: Number
 });
 
+// Model Export
 module.exports = mongoose.model("Review", ReviewSchema);
