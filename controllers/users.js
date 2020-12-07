b1f2da682e3418955f6a373beb54ee15f16986dd
Franchise <tylerreedsullivan@gmail.com>
2020-12-07 16:38:53 -0600

    Added comments.

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,12 @@
+// Imports
 const User = require('../models/user');
 
+// Render Register Form
 module.exports.renderRegisterForm = (req, res) => {
     res.render('users/register');
 }
 
+// Create User
 module.exports.createUser = async (req, res) => {
     try {
         const { email, username, password } = req.body;
@@ -23,16 +26,19 @@ module.exports.createUser = async (req, res) => {
     }
 }
 
+// Render Login Form
 module.exports.renderLoginForm = (req, res) => {
     res.render('users/login');
 }
 
+// User login
 module.exports.login = (req, res) => {
     req.flash('success', 'Welcome back!');
     const redirectUrl = req.session.returnTo || '/stadiums';
     res.redirect(redirectUrl);
 }
 
+// User logout
 module.exports.logout = (req, res) => {
     req.logout();
     req.flash('success', 'Successfully logged out.')
